22e44f41350ea9880ac9b74fbd96c11860741e92
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-07-20 15:29:02 +0530

    explore section updated

--- client/src/Components/ExploreSection.jsx
+++ client/src/Components/ExploreSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import cruiseImg from '../assets/Images/cruise.png'
 import AliceCarousel, { Link } from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { FaLocationDot } from 'react-icons/fa6';
 const ExploreSection = () => {
 
     const [active, setActive] = useState(1)
@@ -12,13 +13,13 @@ const ExploreSection = () => {
             items: 1,
         },
         640: {
-            items: 3
+            items: 1
         },
-        768: {
-            items: 3
+        900: {
+            items: 1
         },
         1024: {
-            items: 3
+            items: 2
         }
 
     }
@@ -31,19 +32,17 @@ const ExploreSection = () => {
 
     const item = trendingList.map((val) => {
         return <>
-            <Link className='flex flex-col items-start bg-white text-black pb-3 w-[12rem] rounded-md  text-[15px] md:text-[16px] lg:text-[17px] '>
-                < img src={val.image} alt="" className='w-full h-[6.5rem]  mb-2 lg:mb-4 object-cover' />
-                <div className='p-2'>
-                    <div className='flex items-start  flex-col justify-between w-full'>
-                        <h3 className='font-bold w-[8.8rem] truncate'>{val.name}</h3>
-                        <p className='w-[8.8rem] truncate'>location</p>
+            <Link className='flex flex-col p-2 items-start bg-white text-black pb-3  rounded-md m-3 w-fit  text-[15px] md:text-[16px] lg:text-[17px] '>
+                <div className='flex items-center justify-center gap-3'>
+                    <div className='bg-[#FFF3E5] p-2 rounded-full'>
+                        <FaLocationDot className='text-[#FF8900] text-[0.95rem]' />
                     </div>
-                    <div className='flex items-center justify-between'>
-                        <strike>2355</strike>
-                        <h4>1335</h4>
-                    </div>
-                    <p>1 room per night</p>
+                    <p>Location</p>
                 </div>
+                <h2>Place name</h2>
+
+                < img src={val.image} alt="" className='sm:w-[24rem] w-[85vw] sm:h-[20rem] h-[16rem]   mb-2 lg:mb-4 object-cover' />
+
 
             </ Link>
         </>
@@ -59,18 +58,20 @@ const ExploreSection = () => {
                 <h5>Recommendation</h5>
                 <h5>Best Place</h5>
             </div>
-            <div className='w-[12.5rem]   sm:w-[20rem] md:w-[37.8rem] flex items-center justify-center '>
-                <AliceCarousel
-                    mouseTracking
-                    autoPlayInterval={2500}
-                    animationDuration={1200}
-                    infinite
-                    responsive={responsive}
-                    disableDotsControls
-                    disableButtonsControls
-                    items={item}
-                    autoPlay
-                />
+            <div >
+                <div className='w-[85vw] sm:w-[24rem] lg:w-[48rem] flex items-center justify-center border  '>
+                    <AliceCarousel
+                        mouseTracking
+                        autoPlayInterval={2500}
+                        animationDuration={1200}
+                        infinite
+                        responsive={responsive}
+                        disableDotsControls
+                        disableButtonsControls
+                        items={item}
+                        autoPlay
+                    />
+                </div>
             </div>
         </div>
     )
