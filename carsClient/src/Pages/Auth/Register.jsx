cafeaf66dbe5dbcaf490e3eac9b24442e05d5987
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-07-24 00:47:59 +0530

    Register form updated

--- carsClient/src/Pages/Auth/Register.jsx
+++ carsClient/src/Pages/Auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom'
 const Register = () => {
 
     const [input, setInput] = useState({
@@ -15,16 +15,16 @@ const Register = () => {
 
     const mainDiv = 'flex flex-col gap-[0.1px]'
 
-    const labelStyle = "text-[0.83rem] tracking-wide text-[#000] font-[500] ml-[0.5px]"
-    const inputStyle = 'border min-w-[18rem] max-w-[20.5rem] w-[87vw] sm:w-[24rem] rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-white text-black'
+    const labelStyle = "text-[0.83rem] tracking-wide text-[#CFCCE4] font-[400] ml-[0.5px]"
+    const inputStyle = 'border border-[#685ED4] min-w-[18rem] max-w-[20.5rem] w-[87vw] sm:w-[24rem] rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-[#3D4056] text-white'
     return (
-        <div className='flex items-center justify-center text-black bg-[#f8f6fc]'>
-            <form action="" className='sm:p-10 p-4  bg-white rounded-md shadow-[0px_0px_10px_#bfbbc7] my-12 flex md:flex-row flex-col w-fit md:gap-10'>
-                <div className=''>
+        <div className='flex items-center justify-center text-white bg-[#f8f6fc]'>
+            <form action="" className='sm:p-10 p-6  bg-[#2F3349] rounded-md shadow-[0px_0px_20px_#3D4056] my-12 flex md:items-end md:flex-row flex-col w-fit md:gap-10'>
+                <div className='flex flex-col gap-2'>
                     <div className='mb-4'>
                         <h2 className='text-[1.8rem] font-semibold tracking-wide '>Registration Form</h2>
-                        <div className='flex items-center'>
-                            <div className='bg-[#685ED4] w-8 h-[5px] rounded-full mr-1'> </div>
+                        <div className='flex items-center mt-2'>
+                            <div className='bg-[#685ED4] w-12 h-[5px] rounded-full mr-1'> </div>
                             <div className='bg-[#685ED4] w-[12px] h-[5px] rounded-full mr-1'></div>
                             <div className='bg-[#FF4C51] w-[5px] h-[5px] rounded-full'></div>
                         </div>
@@ -49,27 +49,47 @@ const Register = () => {
                         <label className={labelStyle} htmlFor="">Car Number</label>
                         <input className={inputStyle} type="text" />
                     </div>
+
                     <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">Experience</label>
-                        <input className={inputStyle} type="number" />
+                        <label className={labelStyle} htmlFor="">Password</label>
+                        <input className={inputStyle} type="password" />
                     </div>
                     <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">Age</label>
-                        <input className={inputStyle} type="number" />
+                        <label className={labelStyle} htmlFor="">Confirm Password</label>
+                        <input className={inputStyle} type="password" />
                     </div>
+
+
                 </div>
 
-                <div className=''>
+                <div className='flex flex-col gap-2 mt-2 md:mt-20'>
+                    <div className='flex  min-w-[18rem] max-w-[20.5rem] w-[87vw] sm:w-[24rem] justify-between'>
+
+                        <div className={`${mainDiv} w-[48%]`}>
+                            <label className={labelStyle} htmlFor="">Age</label>
+                            <input className='border border-[#685ED4] w-full rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-[#3D4056] text-white' type="number" />
+                        </div>
+                        <div className={`${mainDiv} w-[48%]`}>
+                            <label className={labelStyle} htmlFor="">Experience</label>
+                            <input className='border border-[#685ED4] w-full rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-[#3D4056] text-white' type="number" />
+                        </div>
+
+                    </div>
+                    <div className='flex  min-w-[18rem] max-w-[20.5rem] w-[87vw] sm:w-[24rem] justify-between'>
+
+                        <div className={`${mainDiv} w-[48%]`}>
+                            <label className={labelStyle} htmlFor="">Aadhar Card</label>
+                            <input className='border border-[#685ED4] w-full rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-[#3D4056] text-white' type="file" />
+                        </div>
+                        <div className={`${mainDiv} w-[48%]`}>
+                            <label className={labelStyle} htmlFor="">PAN CARD</label>
+
+                            <input className='border border-[#685ED4] w-full rounded-[3px] h-full  px-2 p-[5.5px]  outline-none  text-[0.95rem] tracking-wide resize-none bg-[#3D4056] text-white' type="file" />
+                        </div>
+
+                    </div>
                     <div className={mainDiv}>
                         <label className={labelStyle} htmlFor="">Your photo</label>
-                        <input className={inputStyle} type="file" name="" id="" />
-                    </div>
-                    <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">Aadhar Card</label>
-                        <input className={inputStyle} type="file" name="" id="" />
-                    </div>
-                    <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">PAN CARD</label>
                         <input className={inputStyle} type="file" name="" id="" />
                     </div>
                     <div className={mainDiv}>
@@ -80,15 +100,12 @@ const Register = () => {
                         <label className={labelStyle} htmlFor="">PUC Certificate</label>
                         <input className={inputStyle} type="file" name="" id="" />
                     </div>
-                    <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">Password</label>
-                        <input className={inputStyle} type="password" />
+
+                    <button className='bg-[#685ED4] hover:bg-[#FF4C51] text-white transition-all duration-700 w-full rounded-md p-[6.9px] font-semibold mt-[12px] mb-1'>Sign Up</button>
+
+                    <div className='w-full text-center'>
+                        <p>Already have an account? <Link to={"/login"} className='text-[#FF4C51] font-semibold underline '>Login</Link></p>
                     </div>
-                    <div className={mainDiv}>
-                        <label className={labelStyle} htmlFor="">Confirm Password</label>
-                        <input className={inputStyle} type="password" />
-                    </div>
-                    <button className='bg-[#685ED4] hover:bg-[#FF4C51] text-white transition-all duration-700 w-full rounded-md p-2 font-semibold'>Sign Up</button>
                 </div>
             </form>
         </div>
