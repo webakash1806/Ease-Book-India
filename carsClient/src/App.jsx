f030c59aef85f0ab888905e8adb23608d7391d07
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-07-26 18:44:12 +0530

    Authentication added

--- carsClient/src/App.jsx
+++ carsClient/src/App.jsx
@@ -4,15 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Login from './Pages/Auth/Login'
 import Register from './Pages/Auth/Register'
+import RequireAuth from './Components/Auth/RequireAuth'
 
 const App = () => {
   return (
     <HomeLayout>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route element={<RequireAuth allowedRoles={['RIDER']} />}>
+          <Route path='/' element={<Home />} />
+        </Route>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-
       </Routes>
     </HomeLayout>
   )
